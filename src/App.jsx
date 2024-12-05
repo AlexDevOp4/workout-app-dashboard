1cb47414a9e7c767d1581319fa95da2362560c89
Alex Ashtiany <alex.ashtiany@icloud.com>
2024-12-05 11:28:03 -0500

    commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import "./index.css";
+import "./App.css";
+
+function App() {
+
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailsdfds wind CSS!</h1>
+    </div>
+  );
+}
+
+export default App;
